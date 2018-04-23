ee5a8bfd889f4087e0e77c0bc9f4a3d0a4ae671b
wangbingxin <bingo_wangbingxin@163.com>
2018-04-23 12:19:12 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,119 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    loopImgs:[],
+    moduleOneData: [],
+    moduleTwoData:[],
+    moudleTwoFirst:{},
+    moduleThreeData: [],
+    moduleFourData: [],
+    moudleFourFirst: {},
+    rankList:[]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  toBook: function () {
+    console.log(getCurrentPages())
+    wx.navigateTo({
+      url: '../book/book'
+    })
+  },
+  onLoad: function () {
+    let _this =this
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/h5_img_show?sex=1',
+      success:function(res){
+        _this.setData({
+          loopImgs: res.data.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/mobile_editor_selected?sex=1',
+      success: res=>{
+        _this.setData({
+          moduleOneData: res.data.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/mobile_book_live?sex=1',
+      success: res => {
+        _this.setData({
+          moudleTwoFirst: res.data.data[0],
+          moduleTwoData: res.data.data.slice(1)
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/mobile_editor_recommend?sex=1',
+      success: res => {
+        _this.setData({
+          moduleThreeData: res.data.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/mobile_new_book_recommend?sex=1',
+      success: res => {
+        _this.setData({
+          moudleFourFirst: res.data.data[0],
+          moduleFourData: res.data.data.slice(1)
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wh.yueloo.com.cn/v1.0/click_book?sex=1',
+      success: function (res) {
+        _this.setData({
+          rankList: res.data.data
+        })
+      }
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
